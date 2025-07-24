b6f6165b08268e9271934f8912f0048373f205d9
Josh_dfG <agboolajoshua161@gmail.com>
2025-07-24 03:19:43 +0100

    feat: revert back

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,27 +7,6 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // apply to all routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self';",
-              "frame-ancestors 'self' https://*.walletconnect.org https://secure.walletconnect.org https://secure-mobile.walletconnect.org;",
-              "frame-src 'self' https://*.walletconnect.org https://secure.walletconnect.org https://secure-mobile.walletconnect.org;",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.walletconnect.org;",
-              "connect-src *;",
-              "img-src * data: blob:;",
-            ].join(" "),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
